972df908de44e132da21ec624c2abf134c6f97e9
David N Sakaria <74316852+201086263@users.noreply.github.com>
2021-11-14 11:23:48 +0200

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,17 @@
+
+-- create database
+CREATE DATABASE ComputingAndInformatics;
+
+
+CREATE TABLE CourseOutline(
+ Id INT PRIMARY KEY AUTO_INCREMENT,
+ courseInformation JSON,
+ LeactureInformation JSON,
+ LearningOutcomes JSON,
+ CourseContent JSON,
+ IsSignedByLeacture BOOLEAN,
+ IsApprovedByHoD  BOOLEAN
+);
+
+
+
